9a7f7302a29b3bdef26137d98a9b8c0c7fc34ff3
Tushar Bhardwaj <tusharbhardwaj.dev@gmail.com>
2025-01-18 20:38:07 +0530

    add route for get existing charts and chat id

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -99,6 +99,45 @@ app.post("/room", middleware, async (req, res) => {
     }
 })
 
+app.get("/chats/:roomId", async (req, res) => {
+    try {
+        const roomId = Number(req.params.roomId);
+        console.log(req.params.roomId);
+        const messages = await prismaClient.chat.findMany({
+            where: {
+                roomId: roomId
+            },
+            orderBy: {
+                id: "desc"
+            },
+            take: 50
+        });
+
+        res.json({
+            messages
+        })
+    } catch(e) {
+        console.log(e);
+        res.json({
+            messages: []
+        })
+    }
+    
+})
+
+app.get("/room/:slug", async (req, res) => {
+    const slug = req.params.slug;
+    const room = await prismaClient.room.findFirst({
+        where: {
+            slug
+        }
+    });
+
+    res.json({
+        room
+    })
+})
+
 app.listen(3001, () => {
-    console.log("Server started at 3001");
+    console.log("Server Start at 3001 port");
 });
